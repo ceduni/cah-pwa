14baeb2cd308f69a6c4d545f22168a138b62379c
Louis-Edouard LAFONTANT <lelafontant@yahoo.ca>
2025-03-09 15:33:30 -0400

    manifest json start url updated, service worker relative paht updated

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,7 @@
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/cah-pwa/docs/service-worker.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope: ', registration.scope);
             })
